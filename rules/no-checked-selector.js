b6010cefbfd69f6d6b5ad05711241171abf201a3
Patsy Issa <patsy.issa@gmail.com>
2020-10-13 16:36:50 +0200

    Add no checked selector rule

---
+++ rules/no-checked-selector.js
@@ -0,0 +1,44 @@
+const message =
+  "use assert.dom('.foo').isChecked() instead assert.dom('.foo:checked').exists()";
+
+module.exports = {
+  message,
+
+  create(context) {
+    let sourceCode = context.getSourceCode();
+
+    return {
+      CallExpression(node) {
+        if (node.callee.type !== "MemberExpression") return;
+
+        if (node.callee.object.type !== "Identifier") return;
+        if (node.callee.object.name !== "assert") return;
+        if (node.callee.property.type !== "Identifier") return;
+        if (node.callee.property.name !== "dom") return;
+
+        if (!node.parent) return;
+        if (node.parent.type !== "MemberExpression") return;
+
+        if (node.parent.property.type !== "Identifier") return;
+        if (node.parent.property.name !== "exists") return;
+
+        if (!node.parent.parent) return;
+        if (node.parent.parent.type !== "CallExpression") return;
+
+        if (node.arguments.length !== 1) return;
+        let firstArg = node.arguments[0];
+        if (!firstArg) return;
+        if (firstArg.type !== "Literal") return;
+        if (typeof firstArg.value !== "string") return;
+
+        if (firstArg.value === ":checked") return;
+        if (!firstArg.value.endsWith(":checked")) return;
+
+        context.report({
+          node: node,
+          message,
+        });
+      },
+    };
+  },
+};
---
+++ rules/no-checked-selector.test.js
@@ -0,0 +1,39 @@
+const { RuleTester } = require("eslint");
+
+const rule = require("./no-checked-selector");
+
+let ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+});
+
+ruleTester.run("no-checked-selector", rule, {
+  valid: [
+    "assert()",
+    "assert.foo",
+    "assert.foo('.foo:checked')",
+    "notAssert.dom('.foo:checked')",
+    "assert.dom('.foo:checked').somethingElse()",
+    "assert.dom('.foo:checked').exists",
+    "assert.dom(':checkedfoo').exists()",
+    "assert.dom(':checked').exists()",
+    "assert.dom('.foo:checked', '.bar').exists()",
+    "assert.dom().exists()",
+    "assert.dom(node).exists()",
+    "assert.dom(42).exists()",
+  ],
+  invalid: [
+    {
+      code: "assert.dom('.foo:checked').exists()",
+      errors: [
+        {
+          message: rule.message,
+          column: 1,
+          endColumn: 27,
+        },
+      ],
+    },
+  ],
+});

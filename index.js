42f759243fbb55a643968ed0b7b3cc4652d55031
Tobias Bieniek <tobias.bieniek@gmail.com>
2021-09-16 14:19:24 +0200

    Automatically export all rules (#22)

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  rules: {
-    'no-ok-find': require('./rules/no-ok-find'),
-  },
+  rules: generateRulesMap(),
 };
+
+function generateRulesMap() {
+  let rulesPath = path.join(__dirname, 'rules');
+  let files = fs.readdirSync(rulesPath);
+
+  let rulesMap = {};
+  for (let file of files) {
+    if (file.endsWith('.js') && !file.endsWith('.test.js')) {
+      let ruleName = path.parse(file).name;
+      rulesMap[ruleName] = require(`./rules/${file}`);
+    }
+  }
+  return rulesMap;
+}

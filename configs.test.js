4c14885585c930cf54d9ae525bccabc6cd427cce
Tobias Bieniek <tobias@bieniek.cloud>
2021-09-16 14:19:12 +0200

    Export `recommended` ESLint config

---
+++ configs.test.js
@@ -0,0 +1,18 @@
+describe('configs', () => {
+  it('recommended is stable', () => {
+    // if you change the list of recommended rules, make sure to release this
+    // as a breaking change!!
+
+    expect(require('./index').configs.recommended).toMatchInlineSnapshot(`
+Object {
+  "plugins": Array [
+    "qunit-dom",
+  ],
+  "rules": Object {
+    "qunit-dom/no-checked-selector": "error",
+    "qunit-dom/no-ok-find": "error",
+  },
+}
+`);
+  });
+});
